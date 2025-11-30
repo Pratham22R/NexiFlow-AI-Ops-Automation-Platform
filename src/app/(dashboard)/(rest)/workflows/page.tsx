c01f1314c68a8423588cdc05eb156b4860e65d24
Pratham22R <pratham2262003@gmail.com>
2025-11-30 23:08:42 +0530

    Implemented Workflows CRUD operations

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,9 +1,28 @@
+import {
+  WorkflowsList,
+  WorkflowsConatiner,
+} from "@/features/workflows/components/Workflows";
+import { prefetchWorkflows } from "@/features/workflows/server/prefetch";
 import { requireAuth } from "@/lib/auth-utils";
-import React from "react";
+import { HydrateClient } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 const Page = async () => {
   await requireAuth();
-  return <div>workflow</div>;
+
+  prefetchWorkflows();
+  return (
+    <WorkflowsConatiner>
+      <HydrateClient>
+        <ErrorBoundary fallback={<p>Error!</p>}>
+          <Suspense fallback={<p>Loading....</p>}>
+            <WorkflowsList />
+          </Suspense>
+        </ErrorBoundary>
+      </HydrateClient>
+    </WorkflowsConatiner>
+  );
 };
 
 export default Page;
---
+++ src/components/upgrade-modal.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { authclient } from "@/lib/auth-client";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "./ui/alert-dialog";
+
+interface UpgradeModalProps {
+  open: boolean;
+  onOpenChange: (oopen: boolean) => void;
+}
+
+export const UpgradeModal = ({ open, onOpenChange }: UpgradeModalProps) => {
+  return (
+    <AlertDialog open={open} onOpenChange={onOpenChange}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Upgrade to Pro</AlertDialogTitle>
+          <AlertDialogDescription>
+            You need an active subscription to perform this action. Upgrade to
+            Pro to unlock all the features.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={() => authclient.checkout({ slug: "Nexiflow-Pro" })}
+          >
+            Upgrade Now
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
